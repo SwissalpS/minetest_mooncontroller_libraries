cd0718b665fbef21343ed03570ff7ebc9d6b0140
Luke aka SwissalpS <Luke@SwissalpS.ws>
2023-06-07 01:34:15 +0200

    start a compact vector replacement with jd in mind

--- vector.lua
+++ vector.lua
@@ -1,2 +1,13 @@
+local tV = {}
+tV.new = function(x, y, z, r)
+	return { x = x or 0, y = y or 0, z = z or 0, r = r }
+end
+tV.copy = function(v)
+print(type(v))
+	if 'table' ~= type(v) then return tV.new() end
+	return { x = v.x, y = v.y, z = v.z, r = v.r }
+end
+
+mooncontroller.luacontroller_libraries['vector_basic'] = tV
 mooncontroller.luacontroller_libraries['vector'] = vector
 
