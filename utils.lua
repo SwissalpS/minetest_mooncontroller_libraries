7a12f18a78370c92698b5ca88ffa46cd35d99d7f
Luke aka SwissalpS <Luke@SwissalpS.ws>
2023-06-08 14:53:08 +0200

    fix whitespace

--- utils.lua
+++ utils.lua
@@ -4,12 +4,13 @@ mooncontroller_libs.is_lib_disabled = function(s)
 
 end -- is_lib_disabled
 
+
 local sMP = mooncontroller_libs.base_path
 mooncontroller_libs.dofile_if_enabled = function(s)
 
-    if mooncontroller_libs.disabled_libs[s] then return end
+	if mooncontroller_libs.disabled_libs[s] then return end
 
-    return dofile(sMP .. '/' .. s .. '.lua')
+	return dofile(sMP .. '/' .. s .. '.lua')
 
 end -- dofile_if_enabled
 
